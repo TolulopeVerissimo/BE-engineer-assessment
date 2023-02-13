dd77d558a2bfa8b072e9e4ab1d4fec2ef75a764c
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2023-02-13 13:11:10 -0500

    initial vercel serverless setup

--- app/Models/ingredients.router.ts
+++ app/Models/ingredients.router.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from "express"
 import * as APIService from "./ingredients.service"
 import { Meals } from "./ingredients.interface"
+import * as fs from "fs"
+import * as path from 'path'
 
 export const mealsRouter = express.Router()
 
@@ -9,6 +11,11 @@ mealsRouter.get('/:ingredientName', async (req: Request, res: Response) => {
      const name: string = req.params.ingredientName.toString()
      try{
           const recipes: Meals | void = await APIService.findAll(name)
+          fs.writeFile(
+               path.join('api/responseDump',`${name}.ts`),
+               JSON.stringify(recipes, null, 3),
+               (err)=> err && console.error(err)
+          )
           res.status(200).send(recipes)
      } catch (e:unknown){
           res.status(500).send(e)
