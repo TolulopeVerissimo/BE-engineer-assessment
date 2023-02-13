8dbd3612f9d896cf4b96ca872b6b6462505963fe
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2023-02-13 11:37:30 -0500

    quick setup

---
+++ app/index.ts
@@ -0,0 +1,22 @@
+"use strict";
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { mealsRouter } from "./Models/ingredients.router";
+
+dotenv.config();
+
+if (!process.env.PORT) process.exit(1);
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use("/api", mealsRouter)
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
